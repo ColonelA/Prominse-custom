a346f0651a2a9fe46ac117b1b0effe4ddaa60a08
0027017473 <xiacakl@gmail.com>
2023-04-27 10:29:26 +0800

    add 完善附加提交函数

--- promise.js
+++ promise.js
@@ -7,30 +7,30 @@ const REJECTED = 'REJECTED';
 function resolvePromise(newPromise, x, resolve, reject) {
 
     if (newPromise === x) {
-        return reject(new TypeError('错误，引用一致'));
+        return reject(new TypeError('错误，函数引用一致'));
     }
-  
+
     let called = false;
     if (typeof x === 'object' && x !== null || typeof x === 'function') {
         try {
             let then = x.then;
             if (typeof then === 'function') {
-                then.call(x, y => { 
+                then.call(x, y => {
                     if (called) return;
                     called = true;
                     resolvePromise(newPromise, y, resolve, reject)
-                }, err => { 
+                }, err => {
                     if (called) return;
                     called = true;
                     reject(err)
                 })
             }
 
-        } catch (e) { 
+        } catch (e) {
             reject(x)
         }
 
-    } else { 
+    } else {
         if (called) return;
         called = true;
         // 普通值直接执行 
@@ -85,6 +85,31 @@ class Promise {
 
 
         const newPromise = new Promise((resolve, reject) => {
+
+            // 订阅模式
+            if (this.status == FULFILLED) { // 成功调用成功方法
+                setTimeout(() => {
+                    try {
+                        const x = onFulfilled(this.value);
+                        // 此x 可能是一个promise， 如果是promise需要看一下这个promise是成功还是失败 .then ,如果成功则把成功的结果 调用promise2的resolve传递进去，如果失败则同理
+                        // 总结 x的值 决定是调用promise2的 resolve还是reject，如果是promise则取他的状态，如果是普通值则直接调用resolve
+                        resolvePromise(newPromise, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                }, 0);
+            }
+            if (this.status === REJECTED) { // 失败调用失败方法
+                setTimeout(() => {
+                    try {
+                        const x = onRejected(this.reason);
+                        resolvePromise(newPromise, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                }, 0);
+            }
+
             // 记录状态下，保存任务切片 
             if (this.status === PENDING) {
                 this.onRejectedCallbacks.push(() => {
@@ -96,7 +121,18 @@ class Promise {
                             reject(e)
                         }
                     }, 0)
-                })
+                });
+
+             this.onRejectedCallbacks.push(() => { 
+                 setTimeout(() => { 
+                    try {
+                        const x = onFulfilled(this.reason);
+                        resolvePromise(newPromise, x, resolve, reject);
+                    } catch (e) {
+                        reject(e)
+                    }
+                 })
+             })
             }
 
 
